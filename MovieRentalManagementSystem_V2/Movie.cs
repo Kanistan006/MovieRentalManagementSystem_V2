ff34b90f89cfe531e3c2eb27246ada11a7970068
ut01265 <ut01265@UNICOMTIC26>
2024-10-11 12:00:55 +0530

    Add project files.

---
+++ MovieRentalManagementSystem_V2/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem_V2
+{
+    public class Movie
+    {
+        public string movieId { get; set; }
+        public string title { get; set; }
+        public string director { get; set; }
+        public decimal rentalPrice { get; set; }
+
+    
+
+
+
+
+        public Movie(string movieId, string title, string director, decimal rentalPrice)
+        {
+            this.movieId = movieId;
+            this.title = title;
+            this.director = director;
+            this.rentalPrice = rentalPrice;
+      
+        }
+
+        public Movie()
+        {
+
+        }
+
+
+
+      
+    }
+
+}
